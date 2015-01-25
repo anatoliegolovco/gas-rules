90033f280e4ef1d41e2f26571779c4214d039d8d
Anatolie Golovco <golovcoanatolie@gmail.com>
2015-01-25 23:30:15 +0200

    web gui

---
+++ web/js/main.js
@@ -0,0 +1,43 @@
+var map;
+
+$(document).ready( function init(){
+		
+		map = L.map('map').setView([47.01772, 28.87207], 8);
+		
+		var basemap = L.tileLayer('http://bts{s}.hartamd.com/tiles/{z}/{x}/{y}.png', {
+			minZoom:0,
+			maxZoom: 18,
+			attribution: '&copy; <a href="http://hartamd.com/">HartaMD.com</a>',
+			subdomains: [1,2,3,4]
+		});
+		
+		var beta = L.tileLayer('http://ots1.hartamd.com/tiles.php?z={z}&x={x}&y={y}', {
+			minZoom:0,
+			maxZoom: 17,
+			attribution: '&copy; <a href="http://hartamd.com/">HartaMD.com</a>',
+			//subdomains: [1,2,3,4]
+			tms: true
+		}).addTo(map);
+
+		var gas = L.tileLayer('http://ots1.hartamd.com/tms.php?z={z}&x={x}&y={y}&db=gas', {
+			minZoom:0,
+			maxZoom: 17,
+			attribution: '&copy;',
+			tms: true
+		}).addTo(map);
+		
+		var baseLayers = {
+			"New": beta,
+			"Old": basemap
+		};
+
+		var overlays = {
+			"gas": gas
+		};
+
+		//add controls
+		var hash = new L.Hash(map);
+		L.control.locate().addTo(map);
+		L.control.layers(baseLayers, overlays).addTo(map);
+
+	})
